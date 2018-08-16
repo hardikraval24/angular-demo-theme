c790d86bbf6e6d9ffb867d1440431503fbdb1e39
Hardik Raval <hardik.raval@agileinfoways.com>
2018-08-16 18:56:01 +0530

    first commit

---
+++ src/app/core/search-bar/search-bar.component.ts
@@ -0,0 +1,16 @@
+/* import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'cdk-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit {
+	public bigMenu;
+	@Input() open;
+	constructor() { }
+
+	ngOnInit() {}
+
+}
+ */
